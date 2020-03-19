3c8f280af51952aca548056d6fc5e0aebeacdafd
U Sai Rajesh <srajesh636@gmail.com>
2020-03-20 01:39:18 +0530

    fix(webview) : fix webview crash on android :hammer:

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -1135,6 +1135,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
 
     protected void cleanupCallbacksAndDestroy() {
       setWebViewClient(null);
+      removeAllViews();
       destroy();
     }
 
